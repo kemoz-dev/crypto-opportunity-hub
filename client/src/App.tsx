9ee52104f06c713bb1bfd3bff6e9a0ee6fc9a6a9
kemoz-dev <kemoz@live.co.uk>
2026-08-30 01:35:17 +0300

    feat: make root dashboard decision-first and minimal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,13 @@ import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider, useTheme } from "./contexts/ThemeContext";
+import CleanHome from "./pages/CleanHome";
 import Home from "./pages/Home";
 import { PwaStatusBanner, PwaStatusProvider } from "./pwa/PwaStatus";
 import { PwaMobileNavigation } from "./pwa/PwaMobileNavigation";
 
 function Router() {
-  return <Switch><Route path="/" component={Home} /><Route path="/opportunities" component={Home} /><Route path="/asset/:assetId" component={Home} /><Route path="/decision" component={Home} /><Route path="/404" component={NotFound} /><Route component={NotFound} /></Switch>;
+  return <Switch><Route path="/" component={CleanHome} /><Route path="/opportunities" component={Home} /><Route path="/asset/:assetId" component={Home} /><Route path="/decision" component={Home} /><Route path="/404" component={NotFound} /><Route component={NotFound} /></Switch>;
 }
 
 function ThemedApp() {
